72dbfff5c549ec85fca807f6aca9c9d0cfbcc610
nakul1504 <nakulmathur04@gmail.com>
2025-01-16 12:59:28 +0530

    chore: Added Render's deployment domain in ALLOWED_HOSTS

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['inventory-app-p3s4.onrender.com']
 
 
 # Application definition
@@ -70,8 +70,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'inventory_management.wsgi.application'
 
 
-
-# Get MongoDB connection details from environment variables
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
